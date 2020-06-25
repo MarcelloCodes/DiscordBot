1dad9d4df821d3a942271351f7cb1af4ccbc7e18
MarcelloCodes <41309838+MarcelloCodes@users.noreply.github.com>
2020-06-25 13:39:09 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-exports.GOOGLE_KEY = 'AIzaSyDl08eBnYjj3YoQd5WDtmtCDyD6bTZgNN4';
+exports.GOOGLE_KEY = 'AIzaSyAWLidkEGUVl03iO_A2pSybhVNwDxCXh0o';
+exports.TOKEN = 'NzE4MTY4Njc3Nzk4NjQxNzg0.Xtk9Iw.ro09ib4iwnGJyKeTlBtPUk49PWI';
